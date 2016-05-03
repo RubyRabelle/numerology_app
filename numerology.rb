c9c6a274e655e1e5e54d8b10eeea3dbb7a514604
RubyRabelle <rabelle.rouser@gmail.com>
2016-05-03 17:57:36 -0500

    update to skillcrush version
    changed my version of numerology app to the skill crush version for use
    with the sinatra app

--- numerology.rb
+++ numerology.rb
@@ -1,106 +1,43 @@
-#numerology.rb  9/18/2015 Rabelle.Rouser
-
-#SkillCrush Numerology app 
-
-
-#ask user for birthdate mmddyyyy
-puts "What is your birthdate in MMDDYYYY format?"
-
-str_birthdate = gets
-str_number = str_birthdate [0,8]
-#puts str_number
-i = 0
-magic_number = 0
-first_magic_number = 0
-second_magic_number = 0
-third_magic_number = 0
-
-
-
-#build an array, add birthdate numbers together
-while i < 8 do
-	#puts "Pass #{i} : the string is #{str_number} and the number is #{str_number[i]}"
-	first_magic_number += str_number[i].to_i
-	i += 1
-	#puts "The first magic number is :#{first_magic_number}"
+# Numerology App
+ 
+# 1. ask the user for their birthdate
+puts "What is your birthdate? Please write it like this: MMDDYYYY"
+ 
+# 2. use gets to assign their birthdate to a variable
+birthdate = gets
+ 
+# 3. use array syntax to access each element (number) in the birthdate, convert each one to an integer, and add them all together
+number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
+ 
+# 4. convert the number back to a string so you can use array syntax again, and then follow step 3 to add the two numbers together
+number = number.to_s 
+number = number[0].to_i + number[1].to_i
+ 
+# 5. use an if statement to determine if the number is greater than 9; if it is, reduce again
+if number > 9
+    number = number[0].to_i + number[1].to_i
 end
-
-magic_number = first_magic_number
-
-#check to see if the number is larger than 9 and needs to be reduced
-if first_magic_number > 9
-	#puts "need to recalculate the magic number"
-	#puts "The str_number is #{first_magic_number.to_s}"
-	str_number = first_magic_number.to_s
-
-	second_magic_number = str_number[0].to_i + str_number[1].to_i + str_number[2].to_i
-
-	#puts "The second magic number is #{second_magic_number}"
-	magic_number = second_magic_number
+ 
+# 6. use a case statement to display the correct meaning
+case number
+when 1
+    puts "Your numerology number is #{number}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
+when 2
+    puts "Your numerology number is #{number}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+when 3
+    puts "Your numerology number is #{number}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+when 4
+    puts "Your numerology number is #{number}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
+when 5
+    puts "Your numerology number is #{number}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+when 6
+    puts "Your numerology number is #{number}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+when 7
+    puts "Your numerology number is #{number}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+when 8
+    puts "Your numerology number is #{number}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+when 9
+    puts "Your numerology number is #{number}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+else
+    puts "Uh oh! Your birth path number is not 1-9!"
 end
-
-#check again to see if the number is still larger than 9 and needs reduced again
-if second_magic_number > 9
-	#puts "need to recalculate the magic number"
-	#puts "The str_number is #{second_magic_number.to_s}"
-	str_number = second_magic_number.to_s
-
-	third_magic_number = str_number[0].to_i + str_number[1].to_i + str_number[2].to_i
-
-	#puts "The third magic number is #{third_magic_number}"
-	magic_number = third_magic_number
-end
-
-#puts "The final magic number is: #{magic_number}!"
-
-puts case magic_number
-when 1 
-"Number 1
-
-One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
-
-when 2 
-"Number 2
-
-This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
-
-when  3 
-"Number 3
-
-Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three’s enjoy life and have a good sense of humor. Ruled by Jupiter."
-
-when 4 
-"Number 4
-This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
-
-when 5 
-"Number 5
-
-This is the freedom lover. The number five is an intellectual vibration. These are ‘idea’ people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
-
-when 6 
-"Number 6
-
-This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
-
-when 7 
-"Number 7
-
-This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
-
-when 8 
-	"Number 8
-
-This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
-
-when 9 
-"Number 9
-
-This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
-
-	
-end
-
-
-
-
